56e97d8c40ede5c09a254ce497eedc692f62ae05
phkang0410 <hhheyoni@gmail.com>
2022-08-04 17:35:23 +0900

    bookmark CRUD

--- bookmark/bookmarks/views.py
+++ bookmark/bookmarks/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import redirect, render
 
 from django.shortcuts import HttpResponse
 
@@ -13,3 +11,54 @@ def bookmark_list(request):
     bookmark_list = Bookmark.objects.all()
     context = {'bookmark_list':bookmark_list}
     return render(request, 'templates/bookmark_list.html', context)
+
+
+def bookmark_detail(request, pk):
+    bookmark = Bookmark.objects.get(id=pk)
+    context = {'bookmark' : bookmark}
+    return render(request, 'templates/bookmark_detail.html', context)
+
+
+def bookmark_create(request):
+    # if request.method == 'POST':
+    #     context = {'text':'POST METHOD!!!'}
+    #     return render(request, 'templates/bookmark_create.html', context)
+    # context={'text':'GET METHOD!!!'}    
+    # return render(request, 'templates/bookmark_create.html', context)
+    bookmark = Bookmark()
+    if request.method == 'POST':
+        bookmark.title = request.POST['title']
+        bookmark.url = request.POST['url']
+        bookmark.memo = request.POST['memo']
+
+        bookmark.save()
+
+        return redirect(f'/bookmark/{bookmark.id}/')
+        
+    return render(request, 'templates/bookmark_create.html')
+
+
+def bookmark_update(request, pk):
+    bookmark = Bookmark.objects.get(id=pk)
+    context={'bookmark':bookmark}
+
+    if request.method == 'POST':
+        bookmark.title = request.POST['title']
+        bookmark.url = request.POST['url']
+        bookmark.memo = request.POST['memo']
+
+        bookmark.save()
+
+        return redirect(f'/bookmark/{bookmark.id}/')
+        
+    return render(request, 'templates/bookmark_update.html', context)
+
+
+def bookmark_delete(request, pk):
+    bookmark = Bookmark.objects.get(id=pk)
+
+    if request.method == 'POST':
+        bookmark.delete()
+        return redirect('/bookmark/')
+        
+    return render(request, 'templates/bookmark_delete.html')
