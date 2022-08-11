8c05cf03a12029271eaa1e38e11430f355d4c742
heyoni <74006293+heyoni@users.noreply.github.com>
2022-08-11 16:47:21 +0900

    bookmark 끝!

--- bookmark/bookmarks/views.py
+++ bookmark/bookmarks/views.py
@@ -10,55 +10,116 @@ from bookmarks.models import Bookmark
 def bookmark_list(request):
     bookmark_list = Bookmark.objects.all()
     context = {'bookmark_list':bookmark_list}
-    return render(request, 'templates/bookmark_list.html', context)
+    return render(request, 'bookmark_list.html', context)
 
-
+from django.shortcuts import get_object_or_404
 def bookmark_detail(request, pk):
-    bookmark = Bookmark.objects.get(id=pk)
+    # bookmark = Bookmark.objects.get(id=pk)
+    bookmark = get_object_or_404(Bookmark, id=pk)
     context = {'bookmark' : bookmark}
-    return render(request, 'templates/bookmark_detail.html', context)
+    return render(request, 'bookmark_detail.html', context)
 
-
+from .forms import BookmarkForm 
 def bookmark_create(request):
     # if request.method == 'POST':
     #     context = {'text':'POST METHOD!!!'}
     #     return render(request, 'templates/bookmark_create.html', context)
     # context={'text':'GET METHOD!!!'}    
     # return render(request, 'templates/bookmark_create.html', context)
-    bookmark = Bookmark()
-    if request.method == 'POST':
-        bookmark.title = request.POST['title']
-        bookmark.url = request.POST['url']
-        bookmark.memo = request.POST['memo']
+    # bookmark = Bookmark()
+    # if request.method == 'POST':
+    #     bookmark.title = request.POST['title']
+    #     bookmark.url = request.POST['url']
+    #     bookmark.memo = request.POST['memo']
 
-        bookmark.save()
+    #     bookmark.save()
 
-        return redirect(f'/bookmark/{bookmark.id}/')
+    #     return redirect(f'/bookmark/{bookmark.id}/')
         
-    return render(request, 'templates/bookmark_create.html')
+    # return render(request, 'bookmark_create.html')
+    context = {}
+
+    if request.method == 'POST':
+        form = BookmarkForm(request.POST)
+        if form.is_valid():
+            bookmark = form.save()
+            return redirect('bookmarks:detail', bookmark.id)
+    else:
+        form = BookmarkForm()
+        context['form'] = form
+    return render(request, 'bookmark_create.html',context)
+
+
 
 
 def bookmark_update(request, pk):
     bookmark = Bookmark.objects.get(id=pk)
-    context={'bookmark':bookmark}
+    # context={'bookmark':bookmark}
+    # if request.method == 'POST':
+    #     bookmark.title = request.POST['title']
+    #     bookmark.url = request.POST['url']
+    #     bookmark.memo = request.POST['memo']
+    #     bookmark.save()
+    #     return redirect(f'/bookmark/{bookmark.id}/')
+    # return render(request, 'bookmark_update.html', context)
+
+    context = {}
 
     if request.method == 'POST':
-        bookmark.title = request.POST['title']
-        bookmark.url = request.POST['url']
-        bookmark.memo = request.POST['memo']
-
-        bookmark.save()
-
-        return redirect(f'/bookmark/{bookmark.id}/')
-        
-    return render(request, 'templates/bookmark_update.html', context)
+        form = BookmarkForm(request.POST, instance=bookmark)
+        if form.is_valid():
+            bookmark = form.save()
+            return redirect('bookmarks:detail', bookmark.id)
+    else:
+        form = BookmarkForm(instance=bookmark)
+        context['form'] = form
+    return render(request, 'bookmark_update.html',context)
 
 
 def bookmark_delete(request, pk):
     bookmark = Bookmark.objects.get(id=pk)
-
+    context = {'haha':bookmark}
     if request.method == 'POST':
         bookmark.delete()
         return redirect('/bookmark/')
         
-    return render(request, 'templates/bookmark_delete.html')
+    return render(request, 'bookmark_delete.html', context)
+    
+
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+class BookmarkListView(ListView):
+    # 1. 모델 정하기
+    model = Bookmark
+
+    # 2. context(변수명)정하기
+    context_object_name = 'bookmark_list'
+
+    # 3. 어떤 templates연결한건지 정하기
+    template_name = 'bookmark_list.html'
+
+class BookmarkDetailView(DetailView):
+    model = Bookmark
+    context_object_name = 'bookmark'
+    template_name = 'bookmark_detail.html'
+
+from django.urls import reverse_lazy
+class BookmarkCreateView(CreateView):
+    model = Bookmark
+    # template_name = 'bookmark_create.html'
+    form_class = BookmarkForm
+    success_url = reverse_lazy('bookmarks:list')
+
+class BookmarkUpdateView(UpdateView):
+    model = Bookmark
+    # template_name = 'bookmark_update.html'
+    form_class = BookmarkForm
+    success_url = reverse_lazy('bookmarks:list')
+    context_object_name = 'bookmark'
+
+class BookmarkDeleteView(DeleteView):
+    model = Bookmark
+    context_object_name = 'bookmark'
+    # template_name = 'bookmark_delete.html'
+    success_url = reverse_lazy('bookmarks:list')
