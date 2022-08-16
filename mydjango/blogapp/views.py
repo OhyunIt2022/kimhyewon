f9e8d05eee504fee00d9eade3aa60a4e62d5eec5
heyoni <74006293+heyoni@users.noreply.github.com>
2022-08-16 18:25:54 +0900

    blog 앱 작성 완료

--- mydjango/blogapp/views.py
+++ mydjango/blogapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+
+from .models import Post
+class PostListView(ListView):
+    # 1. 모델 지정하기
+    model = Post
+    # 2. 변수명 지정하기
+    context_object_name = 'posts'
+    # 3. template 지정해주기
+    # template_name = 'blog_list.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
